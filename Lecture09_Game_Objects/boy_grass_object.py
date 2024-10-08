05fbf3fdc847a90ab0335fa38718b86c4c904eff
peachspeed_com <131349605+peachspeed@users.noreply.github.com>
2024-10-08 11:10:25 +0900

    Dril_07 제출(2023184025 이승화)

--- Lecture09_Game_Objects/boy_grass_object.py
+++ Lecture09_Game_Objects/boy_grass_object.py
@@ -1,15 +1,88 @@
 from pico2d import *
+import random
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self): pass
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 90  # 랜덤한 x 좌표에서 시작
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+        if self.x > 800:
+            self.x = 0  # 화면을 넘어가면 다시 왼쪽에서 시작
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+class BallSmall:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599  # 랜덤한 x 좌표에서 상단에서 시작
+        self.speed = random.randint(5, 15)  # 랜덤한 낙하 속도
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        if self.y > 70:  # 바닥에서 10px 위에 멈추기
+            self.y -= self.speed
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+class BallLarge:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599  # 랜덤한 x 좌표에서 상단에서 시작
+        self.speed = random.randint(5, 15)  # 랜덤한 낙하 속도
+        self.image = load_image('ball41x41.png')
+
+    def update(self):
+        if self.y > 80:
+            self.y -= self.speed
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
 
 def reset_world():
     global running
+    global grass
+    global world
+    global boys
+    global balls
 
     running = True
+    world = []
+    grass = Grass()
+    world.append(grass)
+
+    # 10명의 보이를 추가
+    boys = [Boy() for _ in range(10)]
+    world += boys
+
+    # 랜덤하게 20개의 공을 추가 (작은 공과 큰 공)
+    ball_count = 20
+    small_ball_count = random.randint(5, ball_count)  # 작은 공의 개수 랜덤 설정
+    large_ball_count = ball_count - small_ball_count  # 나머지는 큰 공
+
+    balls = [BallSmall() for _ in range(small_ball_count)] + [BallLarge() for _ in range(large_ball_count)]
+    world += balls
 
 def update_world():
-    pass
+    for o in world:
+        o.update()
 
 def render_world():
     clear_canvas()
+    for o in world:
+        o.draw()
     update_canvas()
 
 def handle_events():
